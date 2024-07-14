12ee211ad2330c75d47d0593430cf82406255268
hunte <besscomdark@gmail.com>
2024-07-14 22:11:18 +0800

    新增內容

--- Assets/Scripts/Bubble.cs
+++ Assets/Scripts/Bubble.cs
@@ -1,34 +1,12 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Bubble : MonoBehaviour
 {
-    public int colorIndex;
-    public bool isStuck = false;
-    public Vector2Int gridPosition;
-
-    private GameManager gameManager;
-
-    private void Start()
+    void OnCollisionEnter(Collision collision)
     {
-        gameManager = FindObjectOfType<GameManager>();
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (!isStuck && (collision.gameObject.CompareTag("Bubble") || collision.gameObject.CompareTag("Ceiling")))
+        if (collision.gameObject.tag == "Bubble")
         {
-            isStuck = true;
-            GetComponent<Rigidbody2D>().velocity = Vector2.zero;
-            GetComponent<Rigidbody2D>().isKinematic = true;
-
-            // 計算並設置網格位置
-            gridPosition = gameManager.CalculateGridPosition(transform.position);
-            transform.position = gameManager.GetWorldPosition(gridPosition);
-
-            // 通知遊戲管理器檢查匹配
-            gameManager.CheckForMatches(this);
+            Destroy(gameObject);
         }
     }
 }
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject bubblePrefab;
+    public Transform bubbleParent;
+    public float bubbleSpeed = 5f;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            ShootBubble();
+        }
+    }
+
+    void ShootBubble()
+    {
+        GameObject bubble = Instantiate(bubblePrefab, transform.position, Quaternion.identity, bubbleParent);
+        Rigidbody rb = bubble.GetComponent<Rigidbody>();
+        rb.velocity = transform.forward * bubbleSpeed;
+    }
+}
